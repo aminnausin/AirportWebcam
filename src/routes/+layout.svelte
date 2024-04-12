26c4185d4b21ead9150ddf3035a694fb214b2d89
aminnausin <aminnausin@outlook.com>
2024-04-12 00:47:52 -0400

    add toaster

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
+    import toast, { Toaster } from 'svelte-french-toast';
     import "../app.css";
 </script>
 
+<Toaster 
+    position="bottom-right"
+    reverseOrder={true}
+    toastOptions={
+        {duration:4000}
+    }
+/>
 <slot />
