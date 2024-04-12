4bac9dc07067fd36bfdf825540d3eb577227c807
aminnausin <aminnausin@outlook.com>
2024-04-12 00:47:39 -0400

    transfer radio section to component and implement audio peak meter

---
+++ src/components/Radio.svelte
@@ -0,0 +1,52 @@
+<script>
+    // @ts-nocheck
+	import { toast } from 'svelte-french-toast';
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        try {
+            var myHeaders = new Headers();
+            myHeaders.append("X-API-Key", "0c69eb98e9374ea5a1a86b4e23");
+
+            const res = await fetch('https://api.checkwx.com/metar/CYUL/decoded', {method: 'GET', headers: myHeaders, redirect: 'follow'});
+            const response = await res.json();
+            metar = response.data[0].raw_text;
+        } catch (error) {
+            console.log(error);
+        }
+
+        try {
+            const {webAudioPeakMeter} = await import('$lib');
+            var myMeterElement = document.getElementById('my-peak-meter');
+            var myAudio = document.getElementById('player');
+            var audioCtx = new window.AudioContext();
+            var sourceNode = audioCtx.createMediaElementSource(myAudio);
+            sourceNode.connect(audioCtx.destination);
+            var meterNode = webAudioPeakMeter.createMeterNode(sourceNode, audioCtx);
+            webAudioPeakMeter.createMeter(myMeterElement, meterNode, {});
+            myAudio.addEventListener('play', function() {
+                audioCtx.resume();
+            });
+
+            myAudio.play();
+        } catch (error) {
+            toast.error('Audio Peak Meter Error: ' + error)
+        }
+    });
+
+    let metar = 'CYUL 142300Z 29005KT 15SM FEW060 SCT080 12/07 A2970 RMK SC2AC1 SLP062';
+</script>
+<div class="flex gap-2 flex-col">
+
+    <h3 class="text-sm leading-[110%]" style="font-family: Helvetica, sans-serif">You are listening to: <br>CYUL Tower - Montreal, Quebec, Canada</h3>
+    <h3 class="text-xs leading-[110%] font-mono text-[#adadad]">{metar}</h3>
+    <audio id="player" class="my-2" crossorigin="anonymous" preload="metadata" src="https://s1-bos.liveatc.net/cyul_twr2" controls={true} autoplay={true}></audio>
+    <div id="my-peak-meter" class="w-72 h-12 my-4">
+    </div>
+
+    <div class="font-bold text-red-600 text-sm">
+        If stream doesn't start automatically press the play button
+        <br>
+        If stream stops then <a href="/" class="text-[#0275c2]">reload</a> player then press play
+    </div>
+</div>
