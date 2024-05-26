1e0f18127e2cad56b93ab62b43bdc20fb10f02a6
aminnausin <aminnausin@outlook.com>
2024-05-26 14:36:12 -0400

    update reactive styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,10 +37,12 @@
     <section class="w-full xl:w-10/12 xl:pe-3">
         <Webcam live={liveCam} bind:this={webcam}/>
     </section>
-    <section class="flex w-full xl:w-1/6 xl:flex-wrap ps-3 gap-6 h-full">
+    <section class="flex w-full flex-wrap sm:flex-nowrap xl:w-1/6 xl:flex-wrap ps-0 xl:ps-3 gap-6 h-full">
         <Radio airport={selectedAirport}/>
-        <div class="flex flex-col items-center justify-between shrink-0 w-1/3 xl:w-full ">
-            <div class="flex flex-wrap gap-2 items-center h-fit">
+        <!-- sm:flex-col sm: -->
+        <div class="flex flex-col items-center justify-between sm:w-1/3 xl:w-full gap-y-12 sm:gap-y-0">
+            <!-- w-1/2 sm: pe-2 sm:pe-0 -->
+            <div class="w-full flex flex-wrap gap-y-2 sm:gap-x-2 items-center h-fit "> 
                 <div class="flex gap-1">
                     <h6 class="w-full font-bold text-sm">
                         If the webcam isn't working, the source website may be down. 
@@ -50,11 +52,11 @@
                         
                     </h6>
                 </div>
-                <div class="flex gap-2 items-center justify-between w-full">
+                <div class="flex gap-2 items-center justify-between w-1/2 sm:w-full pe-1 sm:pe-0">
                     <img src={gooWebcamsLogo} alt="Webcam company logo" class="hidden sm:block h-12 object-contain rounded-md">
-                    <button class="font-medium rounded-md bg-neutral-100 p-2 text-black h-fit flex w-full text-center justify-center items-center" on:click={() => {if(webcam) webcam.forceUpdate()}}> <span class="line-clamp-2">Refresh Webcam</span></button>
+                    <button class="font-medium rounded-md bg-neutral-100 p-2 text-black h-fit flex w-full text-center justify-center items-center" on:click={() => {if(webcam) webcam.forceUpdate()}}> <span class="line-clamp-1 sm:line-clamp-2">Refresh Webcam</span></button>
                 </div>
-                <div class='w-full'>
+                <div class='w-1/2 sm:w-full ps-1 sm:ps-0'>
                     <button class="font-medium rounded-md bg-neutral-100 p-2 text-black h-fit flex w-full text-center justify-center items-center" on:click={handleFeedSwap}>
                         <span class="line-clamp-1 ">
                             {#if !liveCam}
@@ -66,8 +68,9 @@
                     </button>
                 </div>
             </div>
-            <div class="w-full flex flex-col gap-2 h-fit xl:mt-4">
-                <h6 class="w-full font-bold text-sm">Select Airport for Radio and Flights</h6>
+            <!-- w-1/2 sm: ps-2 sm:ps-0 h-full sm: -->
+            <div class="w-full flex flex-col gap-2 xl:mt-4 justify-between h-fit">
+                <h6 class="w-full font-bold text-sm sm:line-clamp-1">Select Airport (Radio and Flights)</h6>
                 <div class="flex items-center w-full justify-between gap-2 h-fit">
                     {#each airports as airport, idx}
                     <button class={`font-medium rounded-md ${selectedAirport === airport ? 'bg-red-700 text-white' : ''} bg-neutral-100 hover:bg-red-800 hover:text-white p-2 text-black h-fit line-clamp-1 w-full`} on:click={() => {handleAirportSwap(idx)}}>
